5d7b57254eed4109f45819230d2056d1d0e0445d
Rizul <rizulgoyal23@gmail.com>
2019-10-30 20:09:33 -0400

    Initial Commit

---
+++ Mid_Term_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mid_Term_ios
+//
+//  Created by Rizul goyal on 2019-10-30.
+//  Copyright © 2019 Rizul goyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
